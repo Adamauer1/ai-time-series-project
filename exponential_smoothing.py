a93eaf6a72b7b4e4c4e1c26408f73c903e2e9d64
Adamauer1 <adamauer1@gmail.com>
2024-11-28 22:02:25 +0100

    setting up basic file structure

---
+++ exponential_smoothing.py
@@ -0,0 +1,10 @@
+#  s = ax + (1-a)sp
+class ExponentialSmoothing:
+    def __init__ (self, alpha):
+        print("")
+    
+    def fit (self, data):
+        print("")
+
+    def predict (self, steps=1):
+        print("")
